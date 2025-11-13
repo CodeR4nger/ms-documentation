a38a6e73778d20c828e2caf6572afbbc3d3c218f
CodeR4nger <ignabianchi@gmail.com>
2025-11-13 16:33:57 -0300

    Agregado de tipos de documentos no contemplados

--- ms-documentation/Models/TipoDocumento.cs
+++ ms-documentation/Models/TipoDocumento.cs
@@ -1,10 +1,12 @@
 namespace ms_documentation.Models;
 
-public enum TipoDocumento 
+public enum TipoDocumento
 {
     DNI,
     LibretaCivica,
+    LC,
     LibretaEnrolamiento,
+    LE,
     Pasaporte
 }
 
